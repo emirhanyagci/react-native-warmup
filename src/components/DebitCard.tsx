e4272c5718339418cf67ede970efa0684b452e13
Emirhan Yagci <emirhan.yacis@gmail.com>
2025-07-21 16:28:50 +0300

    Fix DebitCard component ios view

--- src/components/DebitCard.tsx
+++ src/components/DebitCard.tsx
@@ -3,15 +3,17 @@ import LinearGradient from 'react-native-linear-gradient';
 
 export default function DebitCard({ number, expiry, type }: { number: string, expiry: string, type: string }) {
     return (
-        <LinearGradient start={{ x: 0.0, y: 0.0 }} end={{ x: 1, y: 1.0 }} colors={['#1E1D1D', '#535353']} style={styles.card}>
-            <Image source={require("../assets/images/visa-soft.png")} style={styles.bgImage} />
-            <View style={{ gap: 8 }}>
-                <Text style={styles.cardText}>**** **** **** {number.slice(-4)}</Text>
-                <Text style={styles.cardBoldText}>{type}</Text>
-            </View>
-            <View style={styles.cardFooter}>
-                <Text style={styles.cardText}>Exp {expiry}</Text>
-                <Image source={require("../assets/images/visa.png")} />
+        <LinearGradient start={{ x: 0.0, y: 0.0 }} end={{ x: 1, y: 1.0 }} colors={['#1E1D1D', '#535353']} style={{ borderRadius: 20 }} >
+            <View style={styles.card}>
+                <Image source={require("../assets/images/visa-soft.png")} style={styles.bgImage} />
+                <View style={{ gap: 8 }}>
+                    <Text style={styles.cardText}>**** **** **** {number.slice(-4)}</Text>
+                    <Text style={styles.cardBoldText}>{type}</Text>
+                </View>
+                <View style={styles.cardFooter}>
+                    <Text style={styles.cardText}>Exp {expiry}</Text>
+                    <Image source={require("../assets/images/visa.png")} />
+                </View>
             </View>
         </LinearGradient>
     )
@@ -20,9 +22,9 @@ const styles = StyleSheet.create({
     card: {
         height: 220,
         borderRadius: 20,
-        color: "#ffffff",
         padding: 35,
-        justifyContent: "space-between"
+        color: "#ffffff",
+        justifyContent: "space-between",
     },
     bgImage: {
         position: "absolute",
